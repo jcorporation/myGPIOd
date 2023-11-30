f136ae68e07813d04b690b6a012579bc98a15a5b
jcorporation <mail@jcgames.de>
2023-11-30 19:29:46 +0100

    Upd: api doc

--- src/event.h
+++ src/event.h
@@ -19,11 +19,18 @@ enum pfd_types {
     PFD_TYPE_SIGNAL
 };
 
+/**
+ * Maximum number off fds to poll
+ */
 #define MAX_FDS (GPIOD_LINE_BULK_MAX_LINES * 2 + 1)
+
+/**
+ * Struct to hold poll fd data
+ */
 struct t_poll_fds {
-    struct pollfd fd[MAX_FDS];
-    int type[MAX_FDS];
-    unsigned len;
+    struct pollfd fd[MAX_FDS];  //!< file descriptors
+    int type[MAX_FDS];          //!< type of the corrsponding fd
+    unsigned len;               //!< number of file descriptors
 };
 
 bool poll_fd_add(struct t_poll_fds *poll_fds, int fd, short events, int pfd_type);
