9841bd65273c30fa3ff41f7e88947205506195ec
jcorporation <mail@jcgames.de>
2026-01-17 15:21:38 +0100

    Upd: Use a thread for async http calls

--- mygpiod/actions/http.c
+++ mygpiod/actions/http.c
@@ -8,11 +8,12 @@
 #include "mygpiod/actions/http.h"
 
 #include "dist/sds/sds.h"
+#include "lib/mem.h"
 #include "mygpiod/lib/log.h"
 #include "mygpiod/lib/sds_extras.h"
 
 #include <curl/curl.h>
-#include <errno.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -33,9 +34,19 @@ const char *http_methods[] = {
     NULL
 };
 
+/**
+ * Arguments for the curl thread
+ */
+struct t_curl_arguments {
+    sds method;
+    sds uri;
+    sds content_type;
+    sds postdata;
+};
+
+static void free_curl_arguments(struct t_curl_arguments *arg);
 static bool validate_http_method(const char *method);
-static bool fork_call_curl(const char *method, const char *uri, const char *content_type, const char *postdata);
-static bool call_curl(const char *method, const char *uri, const char *content_type, const char *postdata);
+static void *curl_thread(void *thread_arg);
 static size_t catch_output(void *ptr, size_t size, size_t nmemb, sds *output);
 
 // public functions
@@ -66,7 +77,7 @@ bool action_http(const char *cmd) {
 }
 
 /**
- * Makes a http call in a new process
+ * Makes a http call in a new thread
  * @param method HTTP method
  * @param uri HTTP Uri
  * @param content_type Content-Type or NULL
@@ -78,11 +89,52 @@ bool action_http2(const char *method, const char *uri, const char *content_type,
         MYGPIOD_LOG_ERROR("Invalid HTTP method: \"%s\"", method);
         return false;
     }
-    return fork_call_curl(method, uri, content_type, postdata);
+    struct t_curl_arguments *arg = malloc_assert(sizeof(struct t_curl_arguments));
+    arg->method = sdsnew(method);
+    arg->uri = sdsnew(uri);
+    if (content_type != NULL &&
+        postdata != NULL)
+    {
+        arg->content_type = sdsnew(content_type);
+        arg->postdata = sdsnew(postdata);
+    }
+    else {
+        arg->content_type = NULL;
+        arg->postdata = NULL;
+    }
+
+    pthread_t scripts_worker_thread;
+    pthread_attr_t attr;
+    if (pthread_attr_init(&attr) != 0 ||
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) != 0 ||
+        pthread_create(&scripts_worker_thread, &attr, curl_thread, arg) != 0)
+    {
+        MYGPIOD_LOG_ERROR("Can not create http client thread");
+        free_curl_arguments(arg);
+        return false;
+    }
+    return true;
 }
 
 // private functions
 
+/**
+ * Frees a t_curl_arguments struct
+ * @param arg t_curl_arguments struct to free
+ */
+static void free_curl_arguments(struct t_curl_arguments *arg) {
+    sdsfree(arg->content_type);
+    sdsfree(arg->method);
+    sdsfree(arg->postdata);
+    sdsfree(arg->uri);
+    free(arg);
+}
+
+/**
+ * Checks if string is a valid HTTP method
+ * @param method Method string to parse
+ * @return true if it is valid HTTP method, else false
+ */
 static bool validate_http_method(const char *method) {
     const char **p = http_methods;
     while(*p != NULL) {
@@ -95,35 +147,6 @@ static bool validate_http_method(const char *method) {
 }
 
 /**
- * Forks and makes the http call
- * @param method HTTP method
- * @param uri HTTP Uri
- * @param content_type Content-Type or NULL
- * @param postdata data to post or NULL
- * @return true on success, else false
- */
-static bool fork_call_curl(const char *method, const char *uri, const char *content_type, const char *postdata) {
-    errno = 0;
-    int pid = fork();
-    if (pid == 0) {
-        // this is the child process
-        bool rc = call_curl(method, uri, content_type, postdata);
-        if (rc == true) {
-            exit(EXIT_SUCCESS);
-        }
-        exit(EXIT_FAILURE);
-    }
-    else {
-        if (pid == -1) {
-            MYGPIOD_LOG_ERROR("Could not fork: %s", strerror(errno));
-            return false;
-        }
-        MYGPIOD_LOG_DEBUG("Forked process with pid %d", pid);
-        return true;
-    }
-}
-
-/**
  * Makes an HTTP call
  * @param method HTTP method
  * @param uri HTTP Uri
@@ -131,49 +154,52 @@ static bool fork_call_curl(const char *method, const char *uri, const char *cont
  * @param postdata data to post or NULL
  * @return true on success, else false
  */
-static bool call_curl(const char *method, const char *uri, const char *content_type, const char *postdata) {
-    (void) method;
+static void *curl_thread(void *thread_arg) {
+    // Initialize
+    struct t_curl_arguments *arg = (struct t_curl_arguments *)thread_arg;
     curl_global_init(CURL_GLOBAL_ALL);
-    CURL *curl = curl_easy_init();
-    if (curl == NULL) {
-        curl_global_cleanup();
-        return false;
-    }
+    CURL *curl;
+    CURLcode res = CURLE_FAILED_INIT;
     sds resp_header = sdsempty();
     sds resp_body = sdsempty();
-    sds data = sdsnew(postdata);
-    curl_easy_setopt(curl, CURLOPT_URL, uri);
+
+    // Run curl
+    curl = curl_easy_init();
+    if (curl == NULL) {
+        goto out;
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, arg->uri);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (curl_write_callback)catch_output);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &resp_body);
     curl_easy_setopt(curl, CURLOPT_HEADERDATA, &resp_header);
     struct curl_slist *slist = NULL;
-    if (postdata != NULL) {
-        sds header = sdscatfmt(sdsempty(), "Content-type: %s", content_type);
+    if (arg->postdata != NULL) {
+        sds header = sdscatfmt(sdsempty(), "Content-type: %s", arg->content_type);
         slist = curl_slist_append(slist, header);
         if (slist == NULL) {
-            return false;
+            goto out;
         }
         sdsfree(header);
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
-        if (strncmp(postdata, "<</", 3) == 0) {
+        if (strncmp(arg->postdata, "<</", 3) == 0) {
             // read file
-            sds file_path = sdsnew(postdata);
+            sds file_path = sdsdup(arg->postdata);
             sdsrange(file_path, 2, -1);
-            sdsclear(data);
+            sdsclear(arg->postdata);
             int nread;
-            data = sds_getfile(data, file_path, &nread);
+            arg->postdata = sds_getfile(arg->postdata, file_path, &nread);
             sdsfree(file_path);
             if (nread == -1) {
-                return false;
+                goto out;
             }
         }
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, arg->postdata);
     }
     char err_buf[CURL_ERROR_SIZE];
     curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, err_buf);
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, arg->method);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "myGPIOd");
-    CURLcode res = curl_easy_perform(curl);
+    res = curl_easy_perform(curl);
     curl_slist_free_all(slist);
     sdstrim(resp_header, "\r\n");
     sdstrim(resp_body, "\r\n");
@@ -190,30 +216,22 @@ static bool call_curl(const char *method, const char *uri, const char *content_t
     if (res != CURLE_OK) {
         MYGPIOD_LOG_ERROR("HTTP call failed: %s", curl_easy_strerror(res));
         MYGPIOD_LOG_ERROR("Error: %s", err_buf);
-        if (sdslen(resp_header) > 0) {
-            MYGPIOD_LOG_ERROR("%s", resp_header);
-        }
-        if (sdslen(resp_body) > 0) {
-            MYGPIOD_LOG_ERROR("%s", resp_body);
-        }
+        MYGPIOD_LOG_ERROR("Header: %s\nBody: %s", resp_header, resp_body);
     }
     else {
-        if (sdslen(resp_header) > 0) {
-            MYGPIOD_LOG_DEBUG("%s", resp_header);
-        }
-        if (sdslen(resp_body) > 0) {
-            MYGPIOD_LOG_DEBUG("%s", resp_body);
-        }
+        MYGPIOD_LOG_DEBUG("Header: %s\nBody: %s", resp_header, resp_body);
     }
-    curl_easy_cleanup(curl);
+
+    // Cleanup
+    out:
+    if (curl != NULL) {
+        curl_easy_cleanup(curl);
+    }
     curl_global_cleanup();
     sdsfree(resp_header);
     sdsfree(resp_body);
-    sdsfree(data);
-    if (res != CURLE_OK) {
-        return false;
-    }
-    return true;
+    free_curl_arguments(arg);
+    return NULL;
 }
 
 /**
